adbc7fd12e89736a3db786d24e3485ddc8750d21
Chengyou Yu <chengyou.yu@nbi.ku.dk>
2026-09-04 18:59:01 +0200

    Added velocity analyzer.

---
+++ analysis/statistacks.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+
+def plot_v (file, window_size):
+    '''
+    Plot temporal velocity profile of a given file
+    file: path to the file
+    window_size: size of the window to average the velocity over. if None, no averaging is done
+    '''
+    with open(file, 'rb') as f:
+        mask_lst, x_lst, p_lst, q_lst = pickle.load(f)
+    
+
+    # Select DVE cells
+    dve_indices = np.where(mask_lst[-1] == 2)[0]
+
+    # Calculate velocity
+    v_lst = []
+    for i in range(len(x_lst)-1):
+        dve_x_i = x_lst[i][dve_indices]
+        dve_x_i_1 = x_lst[i+1][dve_indices]
+
+        # Calculate velocity
+        v_i = dve_x_i_1 - dve_x_i
+
+        # average over all dve cells
+        v_i = np.mean(v_i)
+        v_lst.append(v_i)
+    
+    if window_size is not None:
+        v_lst = np.convolve(v_lst, np.ones(window_size)/window_size, mode='valid')
+    
+    # Plot velocity profile
+    plt.plot(v_lst)
+    plt.show()
+
+    return v_lst
